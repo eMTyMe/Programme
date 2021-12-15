0b379d6d8285bef75de8fd6976e1ac8e5680771a
eMTyMe <max02eu@gmail.com>
2021-12-15 08:59:21 +0100

    test2

--- src/Rohdateien/KontoException.java
+++ src/Rohdateien/KontoException.java
@@ -6,13 +6,5 @@ public class KontoException extends Exception
 	public KontoException(String message) {
 		super(message);
 	}
-	
-	public KontoException(String message, Throwable cause) {
-		super(message, cause);
-	}
-	
-	public KontoException(Throwable cause) {
-		super(cause);
-	}
-	
+
 }
---
+++ src/net/gobbz/ausdrueckeerw/Potenz.java
@@ -0,0 +1,8 @@
+package net.gobbz.ausdrueckeerw;
+
+public class Potenz
+{
+	
+	
+	
+}
